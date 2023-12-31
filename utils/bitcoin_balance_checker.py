6deaf55742b575f7f976dcc028c17ad49c041101
Ben Weeks <ben.weeks@outlook.com>
2023-12-31 15:27:37 +0000

    aider: The BlockCypher token is now retrieved from an environment variable in `bitcoin_balance_checker.py`.
    # Aider chat conversation:
    
    USER: K< I have created an account with BlockCypher. Let's put the token in an env file variable (Token f9ccea0f46634c2a9fda8f18fb2c4992) and use that in the code.
    
    ASSISTANT: To use the BlockCypher token from an environment variable, we need to modify `utils/bitcoin_balance_checker.py`:
    
    1. Import the `os` module at the top of the file.
    2. Retrieve the token from the environment variable and add it to the API URL.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    utils/bitcoin_balance_checker.py
    ```python
    <<<<<<< SEARCH
    import time
    import requests
    import logging
    =======
    import os
    import time
    import requests
    import logging
    >>>>>>> REPLACE
    ```
    
    utils/bitcoin_balance_checker.py
    ```python
    <<<<<<< SEARCH
    bitcoin_api_url = f"https://api.blockcypher.com/v1/btc/main/addrs/{address}/balance"
    =======
    token = os.getenv('BLOCKCYPHER_TOKEN')
    bitcoin_api_url = f"https://api.blockcypher.com/v1/btc/main/addrs/{address}/balance?token={token}"
    >>>>>>> REPLACE
    ```
    
    Please note that you need to set the `BLOCKCYPHER_TOKEN` environment variable in your system or in the script that runs your Python code. You can do this in a bash shell with the command `export BLOCKCYPHER_TOKEN=f9ccea0f46634c2a9fda8f18fb2c4992` before running your Python script. Alternatively, you can use a .env file and a package like python-dotenv to load the environment variables when your script runs.

--- utils/bitcoin_balance_checker.py
+++ utils/bitcoin_balance_checker.py
@@ -1,3 +1,4 @@
+import os
 import time
 import requests
 import logging
@@ -5,7 +6,8 @@ import logging
 logging.basicConfig(filename='seedphrase_fixer.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def check_bitcoin_balance(address):
-    bitcoin_api_url = f"https://api.blockcypher.com/v1/btc/main/addrs/{address}/balance"
+    token = os.getenv('BLOCKCYPHER_TOKEN')
+    bitcoin_api_url = f"https://api.blockcypher.com/v1/btc/main/addrs/{address}/balance?token={token}"
     try:
         time.sleep(1)  # Add a delay before each API request
         time.sleep(1)  # Add a delay before each API request
