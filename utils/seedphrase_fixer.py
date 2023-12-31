cb6f5c3e7cc2889032bd29950a4bdf5d6ae36135
Ben Weeks <ben.weeks@outlook.com>
2023-12-31 16:47:12 +0000

    aider: Added check for None `addresses` variable to prevent AttributeError.
    # Aider chat conversation:
    
    USER: @BenGWeeks ➜ /workspaces/seedphrase-fixer (main) $ python main.py "inhale sunny adapt between polar final mercy concert mercy appear define cabin" "" 6
    BlockCypher API usage is OK
    Seedphrase: inhale sunny adapt between polar final mercy concert mercy appear define cabin
    Passphrase:
    Replace Index: 6
    Invalid checksum for seedphrase: inhale sunny adapt between polar final mercy concert mercy appear define cabin
    Traceback (most recent call last):
      File "/workspaces/seedphrase-fixer/main.py", line 65, in <module>
        main()
      File "/workspaces/seedphrase-fixer/main.py", line 47, in main
        corrected_seedphrase, balances = fix_seedphrase(args.seedphrase, args.passphrase, args.replace_index)
      File "/workspaces/seedphrase-fixer/utils/seedphrase_fixer.py", line 21, in fix_seedphrase
        balances = {address_type: check_bitcoin_balance(address) for address_type, address in addresses.items()}
    AttributeError: 'NoneType' object has no attribute 'items'
    
    ASSISTANT: The error is occurring because the `addresses` variable is `None`, and the code is trying to call the `items()` method on it. This happens when the `derive_multiple_address_types` function raises a `MnemonicChecksumError`, and `addresses` is set to `None`.
    
    To fix this, we can add a check after the call to `derive_multiple_address_types` to see if `addresses` is `None`, and if so, skip the balance checking and continue with the next candidate word.
    
    Here is the *SEARCH/REPLACE* block:
    
    utils/seedphrase_fixer.py
    ```python
    <<<<<<< SEARCH
        balances = {address_type: check_bitcoin_balance(address) for address_type, address in addresses.items()}
    =======
        if addresses is not None:
            balances = {address_type: check_bitcoin_balance(address) for address_type, address in addresses.items()}
        else:
            balances = {'P2PKH': 0, 'P2SH': 0, 'Bech32': 0}
    >>>>>>> REPLACE
    ```

--- utils/seedphrase_fixer.py
+++ utils/seedphrase_fixer.py
@@ -18,7 +18,10 @@ def fix_seedphrase(seedphrase, passphrase, replace_index=None):
         print(f"{Fore.RED}Invalid checksum for seedphrase: {seedphrase}{Style.RESET_ALL}")
         addresses = None
     #print(f"Addresses: {addresses}")
-    balances = {address_type: check_bitcoin_balance(address) for address_type, address in addresses.items()}
+    if addresses is not None:
+        balances = {address_type: check_bitcoin_balance(address) for address_type, address in addresses.items()}
+    else:
+        balances = {'P2PKH': 0, 'P2SH': 0, 'Bech32': 0}
     #print(f"Balances: {balances}")
     if any(value > 0 for value in balances.values()):
         return seedphrase, balances  # Return balances along with seedphrase
