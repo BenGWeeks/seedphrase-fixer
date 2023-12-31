c1225126926a11cc691f505e5760c769ecd6fcca
Ben Weeks <ben.weeks@outlook.com>
2023-12-31 14:34:45 +0000

    Remove redundant bitcoin balance checking function.

--- utils/bitcoin_address_validation.py
+++ utils/bitcoin_address_validation.py
@@ -2,17 +2,6 @@ from utils.address_derivation import derive_multiple_address_types
 from utils.bitcoin_balance_checker import check_bitcoin_balance
 import requests
 
-
-def check_bitcoin_balance(address):
-    bitcoin_api_url = f"https://api.blockcypher.com/v1/btc/main/addrs/{address}/balance"
-    response = requests.get(bitcoin_api_url)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('final_balance', 0) > 0
-    else:
-        return False
-
-
 def validate_with_bitcoin_address(mnemonic, passphrase):
     addresses = derive_multiple_address_types(mnemonic, passphrase)
     
