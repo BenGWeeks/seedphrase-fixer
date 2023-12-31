48cad29ef3c8945f7322795484b22df21dec8184
Ben Weeks <ben.weeks@outlook.com>
2023-12-31 16:40:44 +0000

    aider: Added try-except block to handle invalid checksums and print error message in red using colorama in `fix_seedphrase` function. Also imported `MnemonicChecksumError` and `Fore` at the beginning of the file.
    # Aider chat conversation:
    
    ASSISTANT: I apologize for the confusion. Let's try again.
    
    To handle invalid checksums, we need to modify the `fix_seedphrase` function in `utils/seedphrase_fixer.py`. We can add a try-except block to catch the `MnemonicChecksumError` and print a message in red using `colorama`. If the checksum is valid, we can then check the balance for the derived addresses.
    
    Here is the *SEARCH/REPLACE* block:
    
    utils/seedphrase_fixer.py
    ```python
    <<<<<<< SEARCH
    addresses = derive_multiple_address_types(seedphrase, passphrase)
    =======
    try:
        addresses = derive_multiple_address_types(seedphrase, passphrase)
    except MnemonicChecksumError:
        print(f"{Fore.RED}Invalid checksum for seedphrase: {seedphrase}{Style.RESET_ALL}")
        return None, None
    >>>>>>> REPLACE
    ```
    
    Please note that you need to import `MnemonicChecksumError` from `bip_utils.utils.mnemonic.mnemonic_ex` and `Fore` from `colorama` at the beginning of the file. Here are the *SEARCH/REPLACE* blocks for these imports:
    
    utils/seedphrase_fixer.py
    ```python
    <<<<<<< SEARCH
    from utils.crypto import is_valid_checksum, BIP39_WORDLIST
    from utils.bitcoin_address_validation import validate_with_bitcoin_address
    from utils.bitcoin_balance_checker import check_bitcoin_balance
    from utils.address_derivation import derive_multiple_address_types
    =======
    from bip_utils.utils.mnemonic.mnemonic_ex import MnemonicChecksumError
    from colorama import Fore
    from utils.crypto import is_valid_checksum, BIP39_WORDLIST
    from utils.bitcoin_address_validation import validate_with_bitcoin_address
    from utils.bitcoin_balance_checker import check_bitcoin_balance
    from utils.address_derivation import derive_multiple_address_types
    >>>>>>> REPLACE
    ```
    
    Please apply these changes and let me know if you encounter any issues.

--- utils/seedphrase_fixer.py
+++ utils/seedphrase_fixer.py
@@ -1,3 +1,5 @@
+from bip_utils.utils.mnemonic.mnemonic_ex import MnemonicChecksumError
+from colorama import Fore
 from utils.crypto import is_valid_checksum, BIP39_WORDLIST
 from utils.bitcoin_address_validation import validate_with_bitcoin_address
 from utils.bitcoin_balance_checker import check_bitcoin_balance
@@ -11,7 +13,11 @@ def fix_seedphrase(seedphrase, passphrase, replace_index=None):
 
     # Check if the original seedphrase has a balance
     try:
-        addresses = derive_multiple_address_types(seedphrase, passphrase)
+        try:
+            addresses = derive_multiple_address_types(seedphrase, passphrase)
+        except MnemonicChecksumError:
+            print(f"{Fore.RED}Invalid checksum for seedphrase: {seedphrase}{Style.RESET_ALL}")
+            return None, None
     except MnemonicChecksumError:
         print(f"{Fore.RED}Invalid checksum for seedphrase: {seedphrase}{Style.RESET_ALL}")
         return None, None
