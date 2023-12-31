13ae431c6c117620ce95417f5c08768855987300
Ben Weeks <ben.weeks@outlook.com>
2023-12-31 14:56:53 +0000

    Refactored bitcoin balance checker to handle HTTP errors and log them.

--- utils/bitcoin_balance_checker.py
+++ utils/bitcoin_balance_checker.py
@@ -7,11 +7,11 @@ def check_bitcoin_balance(address):
     bitcoin_api_url = f"https://api.blockcypher.com/v1/btc/main/addrs/{address}/balance"
     try:
         response = requests.get(bitcoin_api_url)
-        response.raise_for_status()  # This will raise an exception for HTTP errors
+        response.raise_for_status()
         data = response.json()
         return data['final_balance']
     except requests.exceptions.HTTPError as http_err:
-        logging.error(f"HTTP error occurred: {http_err}")  # INPUT_REQUIRED {The Bitcoin API endpoint URL might change; ensure it's the correct endpoint for retrieving balances}  
+        logging.error(f"HTTP error occurred: {http_err}")
     except Exception as err:
         logging.error(f"An error occurred: {err}")
     return None
