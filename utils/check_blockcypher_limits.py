43f73148289f1268de1d7002dae40e678629ebf4
Ben Weeks <ben.weeks@outlook.com>
2023-12-31 15:51:58 +0000

    Refactored token retrieval and configuration loading.

--- utils/check_blockcypher_limits.py
+++ utils/check_blockcypher_limits.py
@@ -1,9 +1,18 @@
 import os
 import requests
 import json
+from decouple import Config, Csv
 
-# Load BlockCypher token from environment variables
-token = os.getenv('BLOCKCYPHER_TOKEN')
+#  Create a Config object
+config = Config()
+
+# Load the .env file from the parent folder
+config.read_dotenv('../.env')  # Adjust the path as needed
+
+# Retrieve the value of BLOCKCYPHER_TOKEN
+token = config('BLOCKCYPHER_TOKEN')
+
+print("Token:", token)
 
 def check_limits():
     # Send a GET request to the BlockCypher API
@@ -32,40 +41,3 @@ def check_limits():
 
     # Return a boolean indicating whether the usage is OK or not
     return data['hits']['total_hits'] <= data['limits']['total_limits']
-
-if __name__ == '__main__':
-    check_limits()
-import os
-import requests
-import json
-
-# Load BlockCypher token from environment variables
-token = os.getenv('BLOCKCYPHER_TOKEN')
-
-def check_limits():
-    # Send a GET request to the BlockCypher API
-    response = requests.get(f"https://api.blockcypher.com/v1/tokens/{token}")
-
-    # Raise an exception if the request was unsuccessful
-    response.raise_for_status()
-
-    # Parse the JSON response
-    data = response.json()
-
-    # Print the current usage and limits
-    print(f"Token: {data['token']}")
-    print("Limits:")
-    for key, value in data['limits'].items():
-        print(f"  {key}: {value}")
-    print("Current usage:")
-    for key, value in data['hits'].items():
-        print(f"  {key}: {value}")
-    print("Usage history:")
-    for item in data['hits_history']:
-        print(f"  Time: {item['time']}")
-        for key, value in item.items():
-            if key != 'time':
-                print(f"    {key}: {value}")
-
-if __name__ == '__main__':
-    check_limits()
